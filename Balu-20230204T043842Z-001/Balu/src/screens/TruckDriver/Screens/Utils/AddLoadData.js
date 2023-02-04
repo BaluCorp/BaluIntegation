f33812e85fcb42fa9fbd2a8a6d7ca154885736e3
BaluCorp <124419163+BaluCorp@users.noreply.github.com>
2023-02-04 11:07:34 +0530

    remove

--- Balu-20230204T043842Z-001/Balu/src/screens/TruckDriver/Screens/Utils/AddLoadData.js
+++
@@ -1,8 +0,0 @@
-const Categ = [
-  { item: "Concrete" },
-  { item: "Red Sand" },
-  { item: "White Sand" },
-  { item: "Silica Sand" },
-  { item: "Bricks" },
-];
-// const
--- Balu-20230204T043842Z-001/Balu/src/screens/TruckOwner/Routes/HomePageRoutes.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-import React from "react";
-import AllLoad from "../Screen/HomeRoutes/AllLoad";
-
-const HomePageRoutes = () => {
-  return null;
-};
-
-export default HomePageRoutes;
